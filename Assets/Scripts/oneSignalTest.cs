2dd661e3434abaec65cd30fe857fc70187dfa86f
LaurieMcCulloch <laurie.mcculloch@ntlworld.com>
2021-07-24 08:47:19 +0100

    One Signal Notifications working

--- Assets/Scripts/oneSignalTest.cs
+++ Assets/Scripts/oneSignalTest.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class oneSignalTest : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Uncomment this method to enable OneSignal Debugging log output 
+        // OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+        OneSignal.StartInit("96b7fdfd-f0ac-4730-b91d-3c563dc7615c")
+          .HandleNotificationOpened(OneSignalHandleNotificationOpened)
+          .Settings(new Dictionary<string, bool>() {
+            { OneSignal.kOSSettingsAutoPrompt, false },
+            { OneSignal.kOSSettingsInAppLaunchURL, false } })
+          .EndInit();
+
+        OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+
+        // iOS - Shows the iOS native notification permission prompt.
+        //   - Instead we recomemnd using an In-App Message to prompt for notification 
+        //     permission to explain how notifications are helpful to your users.
+        OneSignal.PromptForPushNotificationsWithUserResponse(OneSignalPromptForPushNotificationsReponse);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Gets called when the player opens a OneSignal notification.
+    private static void OneSignalHandleNotificationOpened(OSNotificationOpenedResult result)
     {
-        
+        Debug.Log("Notification Opened");
+    }
+
+    // iOS - Fires when the user anwser the notification permission prompt.
+    private void OneSignalPromptForPushNotificationsReponse(bool accepted)
+    {
+        // Optional callback if you need to know when the user accepts or declines notification permissions.
+        Debug.Log("Notification Permission Accepted? " + accepted.ToString()) ;
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # OneSig
- 3rd attempt at One Signal
+Project with OneSignal push notifications. The goal is to see if OneSignal notifications can be triggered from deltaDNA webhooks.
 
-## Development Steps
+## One Signal Setup
+The first step is to integrate One Signa and send a simple push notification.
+
 1. Created empty Unity 2018.4 project
 2. Created 'Apple Distribution' Certificate following [Unity publishing for IOS Tutorial](https://learn.unity.com/tutorial/publishing-for-ios#5ecf9859edbc2a001f63c65b)
 3. Unity - Switch Build Settings to IOS
 4. Downloaded P12 key (pwd N0tifyM3!)
 5. Created seperate Apple Push (Sandox and Production) Certificate followin [One Signal Guide](https://documentation.onesignal.com/docs/generate-an-ios-push-certificate)
 6. Downloaded P12 key (pwd N0tifyM3!)
-7. Imported One Signal SDK
+7. Imported One Signal SDK into Unity
+8. Added OneSignal example code and AppID
+9. Created new notification![New Notification 001](Images/new-notification-001.png)
+10. Received new notification 
+![New Notification 001 Received](Images/new-notification-001-received.png)
+
+##
